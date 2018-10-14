a17a37a33404efa104c31e0ed09bbd340a7d24cd
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-10-14 09:16:35 +0100

    chore: create DRF app

--- impetus_api/impetus_api/settings.py
+++ impetus_api/impetus_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'presets',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -78,6 +80,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 
