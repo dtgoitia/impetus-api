b6dc3f1edcd797140d71521b35212aa5853da47d
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-10-14 18:58:45 +0100

    feat: Add Preset model
    Add Preset model and migrate the database.

--- impetus_api/presets/models.py
+++ impetus_api/presets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Preset(models.Model):
+    """
+    Preset Model.
+
+    Defines the attributes of a preset
+    """
+
+    name = models.CharField(max_length=100)
+    summary = models.CharField(max_length=300)
+
+    def __str__(self):
+        """Preset console representation."""
+        return f"<Preset '{self.name}' >"
