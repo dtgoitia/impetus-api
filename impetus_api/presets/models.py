10501d2e39e555215da26445d1d4063bb2548e6a
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-10-22 19:44:46 +0100

    WIP
    - Create a one-to-many relationship in the models and save it
      succesfully to the DB.
    - Create a generic relationship and save part of it to the DB.
    
    Next task: carry on implementing the Preset models.

--- impetus_api/presets/models.py
+++ impetus_api/presets/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import fields
 
 
 class Preset(models.Model):
@@ -11,6 +13,86 @@ class Preset(models.Model):
     name = models.CharField(max_length=100)
     summary = models.CharField(max_length=300, blank=True, default='')
 
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = fields.GenericForeignKey('content_type', 'object_id')
+
     def __str__(self):
         """Preset console representation."""
         return f"name='{self.name}' summary='{self.summary}' >"
+
+
+class Entry(models.Model):
+    """
+    Entry Model.
+
+    Defines the attributes of a entry
+    """
+
+    LOOP = 0
+    TIMER = 1
+    ENTRY_TYPES = (
+        (LOOP, 'Loop'),
+        (TIMER, 'Timer'),
+    )
+
+    entry_type = models.IntegerField(choices=ENTRY_TYPES)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = fields.GenericForeignKey()
+
+    def __str__(self):
+        """Entry console representation."""
+        return f"entry wehey!!"
+
+
+class Timer(models.Model):
+    """
+    Timer Model.
+
+    Defines the attributes of a timer entry.
+    """
+
+    description = models.CharField(max_length=100)
+    time = models.PositiveIntegerField(default=1)
+    is_work = models.BooleanField(default=True)
+    pause = models.BooleanField(default=False)
+
+    entries = fields.GenericRelation(Entry)
+
+
+class Loop(models.Model):
+    """
+    Loop Model.
+
+    Defines the attributes of a loop entry.
+    """
+
+    description = models.CharField(max_length=100)
+    rounds = models.PositiveIntegerField(default=1)
+    # entries = models.ForeignKey(Timer, on_delete=models.CASCADE)
+
+    entries = fields.GenericRelation(Entry)
+
+
+def create_timer(description='Default timer description', time=5000, is_work=True, pause=False):
+    timer = Timer(description=description, time=time, is_work=is_work, pause=pause)
+    return timer
+
+
+def create_loop(description='Default loop description', rounds=3):
+    loop = Loop(description=description, rounds=rounds)
+    return loop
+
+
+# from presets.models import Timer, Loop, Entry, create_loop, create_timer
+# t1 = create_timer()
+# t1.save()
+# t2 = create_timer()
+# t2.save()
+# t3 = create_timer()
+# t3.save()
+# l1 = create_loop()
+# l1.save()
+# preset = Preset(name='My preset', summary='My preset summary')
